ceb1ccec03ac443e6a0000389d2dd03078f866d5
Tobias Happ <tobias.happ@gmx.de>
2017-04-15 00:12:51 +0200

    Refactors controller

--- app/controllers/snippets.js
+++ app/controllers/snippets.js
@@ -11,27 +11,27 @@ const add = (request, response) => {
         return;
     }
 
-    service.keyExists(snippet.key)
-        .then((result) => {
-            if (result) {
-                response.status(422).json({
-                    error: "Key already defined."
-                });
-                return;
-            }
+    handlePromise(response,
+        service.keyExists(snippet.key)
+            .then((result) => {
+                if (result) {
+                    response.status(422).json({
+                        error: "Key already defined."
+                    });
+                    return;
+                }
 
-            return service.create(snippet);
-        })
-        .then((result) => {
-            response.json(result);
-        })
-        .catch((error) => {
-            response.status(500).json({error});
-        });
+                return service.create(snippet);
+            })
+    );
 };
 
 const getAll = (request, response) => {
-    service.getAll()
+    handlePromise(response,service.getAll());
+};
+
+const handlePromise = (response, promise) => {
+    promise
         .then((result) => {
             response.json(result);
         })
