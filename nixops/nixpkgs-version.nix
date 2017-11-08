1bef2d58671976f00908be4cbed905b733881c8d
Tobias Happ <tobias.happ@gmx.de>
2017-11-08 16:05:33 +0100

    Updates dependencies

--- nixops/nixpkgs-version.nix
+++ nixops/nixpkgs-version.nix
@@ -9,6 +9,6 @@
 #   5. Get SHA256 hash of URL contents with `nix-prefetch-url --unpack <url>`.
 
 {
-  url    = "https://d3g5gsiof5omrk.cloudfront.net/nixos/17.09/nixos-17.09.1547.2231575a7d/nixexprs.tar.xz";
-  sha256 = "1ink75hx8hfp3a4lgkiy32m4gm020y7sajbzbgmw73qy9fs5zx6b";
+  url    = "https://d3g5gsiof5omrk.cloudfront.net/nixos/17.09/nixos-17.09.2023.57554d5ece/nixexprs.tar.xz";
+  sha256 = "12izxwm7ns74g06z9106dllf4lvd9qbq99zyyi988xs69w0rg1m1";
 }
